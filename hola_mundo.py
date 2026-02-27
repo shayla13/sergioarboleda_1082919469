e433f69a8812f69e7f27b582918e40adf923b481
Estudiante <student@example.com>
2026-02-27 10:07:12 -0500

    Initial commit

---
+++ hola_mundo.py
@@ -0,0 +1,16 @@
+Nombre = "xavi"
+print(f"hola mundo {Nombre}")
+edad = 18
+if edad >= 18:
+    print("eres mayor de edad")
+else:
+    print("eres menor de edad")
+
+if edad < 13:
+    print("eres un niño")
+elif edad < 18:
+    print("eres un adolescente")
+elif edad < 65:
+    print("eres un adulto")
+else:
+    print("eres un adulto mayor")
