07565fb551411cfad78d756731c15913d82b5ee8
Camilo Moreno <camilo.moreno02@usa.edu.co>
2026-03-02 07:46:20 -0500

    taller for

--- taller-1082919469.py
+++ taller-1082919469.py
@@ -2,4 +2,9 @@ edad =25
 if edad >= 18:
     print("Eres mayor de edad")
 else:
-    print("Eres menor de edad")  
+    print("Eres menor de edad") 
+
+
+#Bucle
+for i in range(5):
+    print("Hola mundo")
